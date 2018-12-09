253724568ffd421630fed00612e39e554aa9eb5d
Hiroshi Sugawara <countdown3to0@gmail.com>
2018-12-09 21:06:05 +0900

    素数判定を追加

---
+++ Dojo/FsDojo/PrimeNumber.fs
@@ -0,0 +1,11 @@
+﻿module PrimeNumber
+
+let IsPrimeNumber = function
+    | 1 -> false
+    | n when n < 1 -> false
+    | n -> 
+        let rec isPrime = function
+        | x when n = x -> true
+        | x when (n%x) = 0 -> false
+        | x -> isPrime (x+1)
+        isPrime 2
